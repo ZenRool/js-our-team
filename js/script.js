11e4e41f67a8cea733e3855db5e787cd320f03a8
Zenrool <lorenzopapperini@hotmail.it>
2022-04-28 17:44:33 +0200

    Bonus 3, aggiungo persone data con gli input html, creo la carta e la stampo nel dom (con la funzione fatta prima) , ed aggiungo l'oggetto nell'array team

--- js/script.js
+++ js/script.js
@@ -36,13 +36,35 @@ const team = [
 // devo un caffè a chiunque abbia inventato il forEach | commento serio per ogni elemento in team faccio printPerson dando come input 
 team.forEach(printPerson);
 team.forEach(domPrintPerson);
+team.forEach(printCard);
+// Bonus 3
+const btn = document.getElementById("addMemberButton");
+btn.addEventListener ("click", () =>{
+    const nameInput = document.getElementById("name").value;
+    const roleInput = document.getElementById("role").value;
+    const imageInput = document.getElementById("image").value;
+    const person = {
+        name: 'noName',
+        role: 'noRole',
+        image: 'undefind' 
+    };
+    person.name = nameInput;
+    person.role = roleInput;
+    person.image = imageInput;
+    // stampa la carta creata 
+    printCard(person);
+
+    //e metto la persona nell'array del team 
+    team.push(person);
+});
+
 // Prima millestone
 // Funzione che stampa tutte le carratteristiche dei elementi di team  nel log
 function printPerson(person) {
     console.log("Nome:", person.name, "; Ruolo:", person.role, "; Indirizzo immagine:", person.image);
 }
 //fine prima millestone
-//Seconda milestone
+//Seconda milestone e Bonus 1
 // Funzione che stampa tutte le carratteristiche dei elementi di team  nel dom dato in input
 function domPrintPerson(person) {
     //Element dove mettere i div che contengono le stringhe
@@ -59,3 +81,33 @@ function domPrintPerson(person) {
     stringDom.append(str);   
 
 }
+//fine seconda milestone e Bonus 1
+// inizio carte Bonus 2
+
+// Come è fatta la carta 
+// .team-card -> (.card-image -> img) .card-tetx -> (h3(nome) p (ruolo) )
+// funzione crea carta e mettila nel dom / sempre person come input
+function printCard(person) {
+    //Element del dom che contiene le carte
+    const domCards = document.getElementsByClassName("team-container")[0];
+    // creo il div card 
+    const card  = document.createElement("div");
+    card.classList.add("team-card");
+
+    const cardImage  = document.createElement("div");
+    cardImage.classList.add("card-image");
+    cardImage.innerHTML = `<img src="img/${person.image}" alt="immagine di ${person.name}">`
+
+    const cardText  = document.createElement("div");
+    cardText.classList.add("card-text");
+    cardText.innerHTML = `
+        <h3>${person.name}</h3>
+        <p>${person.role}</p>
+    `
+    card.append(cardImage);
+    card.append(cardText);
+    console.log(card);
+    domCards.append(card);
+
+}
+
